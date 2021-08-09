6cf0845d42f77a522ff888131c980773e4e3fbbc
vit <miptsu@gmail.com>
2021-08-09 13:50:34 +0300

    feat: 'evolution' speed control by 's' and 'f' keys feat: fps stat out

--- run.cpp
+++ run.cpp
@@ -28,27 +28,41 @@ int main(){
 	bool record = false;
 	//
 	auto cc = cuCalc( img.data, w, h, scale, a);
-	int key=32, lastKey=0;
-	for(size_t cou=0; key!=27 && cou<1000; cou++){
+	int key=32, lastKey=0, delay=20;
+	double avrgDt=0;
+	float da = 1e-5;
+	for(size_t cou=0; key!=27; cou++){
+		const auto t0 = chrono::steady_clock::now();
 		imshow("fractal", colorize(img, imgC));
 		if(record && vw.isOpened()){ vw << imgC;}
 		//
-		if((char)lastKey == 'a'){       scale *= 0.99;} // cout << scale<<endl;
+		if((char)lastKey == 'a'){       scale *= 0.99;}
 		else if((char)lastKey == 'z'){  scale *= (1/0.99);}
 		else if((char)key == 'r'){      record = !record; cout<<"record state: "<<record<<endl;}
 		else if(lastKey == keyLeft){    dx +=10;}
 		else if(lastKey == keyRight){   dx -= 10;}
 		else if(lastKey == keyUp){      dy +=10;}
 		else if(lastKey == keyDown){    dy -= 10;}
-		else if(lastKey == keyPgup){    a += 1e-5;}
-		else if(lastKey == keyPgdown){  a -= 1e-5;}
+		else if(lastKey == keyPgup){    a += da;}
+		else if(lastKey == keyPgdown){  a -= da;}
+		if((char)key == 'f'){           da *= 2;}
+		else if((char)key == 's'){      da *= 1./2;}
+		//
 		cc.recalc(img.data, scale, dx, dy, a);
 		//
 		if(key==32){ key = waitKey(0);}
-		else { key = waitKey(10);}
-		if(key>0){ lastKey = key; cout<<"key = "<<key<<endl;}
+		else {
+			const double dt = (double)chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now()-t0).count();
+			if(cou==0){ avrgDt = dt;
+			} else { avrgDt = 0.95*avrgDt + 0.05*dt;}
+			key = waitKey(dt>=delay? 1 : (int)(1 + delay-dt) );}
+		//
+		if(key>0 && (char)key !='f' && (char)key !='s'){ // -1 if no key pressed
+			lastKey = key; cout<<"key = "<<key<<endl;
+		}
+		if(cou>0 && cou%(int)(1000/delay)==0){ cout<< "fps = " << 1000/(avrgDt + DBL_EPSILON) << endl; }
 	}
 
-	return 1;
+	return 0;
 }
 
